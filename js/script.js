61c0622083159ca291982d03804dab524e5ce499
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-03-29 19:32:08 -0400

    config: Configure buttons
    Configure the event listener to work on the two buttons.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ let p2sec = 60;
 let playing = false;
 let currentPlayer = 0;
 const panel = document.querySelector('.player');
-const startBttn = document.querySelector('.timer__start-bttn');
-const resetBttn = document.querySelector('.timer__reset-bttn');
+const buttons = document.querySelectorAll('.bttn');
 const p1Min = Number(document.querySelector('.p1-minutes').textContent);
 const p2Min = Number(document.querySelector('.p2-minutes').textContent);
 
@@ -46,15 +45,14 @@ const startTimer = () => {
     let timer = setInterval(function() {
         if (currentPlayer === 0) {
             if (playing) {
+                buttons[0].disabled = true;
                 p1time.minutes = parseInt(p1time.getMinutes('min1'), 10);
-                console.log(p1time.minutes);
                 if (p1sec === 60) {
                     p1time.minutes = p1time.minutes - 1;
                 }
-                console.log(p1time.minutes);
                 p1sec = p1sec - 1;
-                document.getElementById('sec1').innerHTML = padZero(p1sec);
-                document.getElementById('min1').innerHTML = padZero(p1time.minutes);
+                document.getElementById('sec1').textContent = padZero(p1sec);
+                document.getElementById('min1').textContent = padZero(p1time.minutes);
                 if (p1sec === 0) {
                     if (p1sec === 0 && p1time.minutes === 0) {
                         clearInterval(timer);
@@ -62,24 +60,22 @@ const startTimer = () => {
                     }
                     p1sec = 60;
                 }
-            } else {
-                if (playing) {
+            }
+        } else {
+            if (playing) {
                 p2time.minutes = parseInt(p2time.getMinutes('min2'), 10);
-                console.log(p2time.minutes);
                 if (p2sec === 60) {
-                    p2time.minutes = p2time.minutes - 2;
+                    p2time.minutes = p2time.minutes - 1;
                 }
-                console.log(p2time.minutes);
                 p2sec = p2sec - 1;
-                document.getElementById('sec2').innerHTML = padZero(p2sec);
-                document.getElementById('min2').innerHTML = padZero(p2time.minutes);
+                document.getElementById('sec2').textContent = padZero(p2sec);
+                document.getElementById('min2').textContent = padZero(p2time.minutes);
                 if (p2sec === 0) {
                     if (p2sec === 0 && p2time.minutes === 0) {
-                        clearInterval(timer);
-                        playing = false;
-                    }
-                    p2sec = 60;
+                    clearInterval(timer);
+                    playing = false;
                 }
+                p2sec = 60;
                 }
             }
         }
@@ -89,7 +85,15 @@ const startTimer = () => {
 
 panel.addEventListener('click', swapPlayer);
 
-startBttn.addEventListener('click', startTimer);
+for (let btn = 0; btn < buttons.length; btn++) {
+    buttons[btn].addEventListener('click', () => {
+        if (buttons[btn].textContent === 'START') {
+            startTimer();
+        } else {
+            location.reload(true);
+        }
+    });
+}
 
 document.addEventListener('keypress', event => {
     if (event.keyCode === 32 || event.which === 32) {
