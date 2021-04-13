00e5a7680144fa585d71aba0210123c84d92233b
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-04-12 22:58:14 -0400

    config: Improve comments
    * Make comments and variables more readable and easier to
    understand.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+
 let playing = false;
 let currentPlayer = 1;
 const timerPanel = document.querySelector('.player');
 const buttons = document.querySelectorAll('.bttn');
+// Sound effects for project.
 const timesUp = new Audio('audio/460133__eschwabe3__robot-affirmative.wav');
 const click = new Audio('audio/561660__mattruthsound.wav');
 
@@ -20,7 +22,6 @@ class Timer {
     constructor(player, minutes) {
         this.player = player;
         this.minutes = minutes;
-        //this.seconds = 60;
     }
     getMinutes(timeId) {
         return document.getElementById(timeId).textContent;
@@ -34,13 +35,16 @@ let p2time = new Timer('min2', document.getElementById('min2').textContent);
 // Swap player's timer after a move (player1 = 1, player2 = 2).
 const swapPlayer = () => {
     if (!playing) return;
+    // Toggle the current player.
     currentPlayer = currentPlayer === 1 ? 2 : 1;
+    // Play the click sound.
     click.play();
 }
 
 
-// Warn player if time drops below one minute and thirty seconds.
+// Warn player if time drops below thirty seconds.
 const timeWarning = (player, min, sec) => {
+    // Change the numbers to red during the last 30 seconds.
     if (min < 1 && sec <= 30) {
         if (player === 1) {
             document.querySelector('.player-1 .player__digits').style.color = '#CC0000';
@@ -57,10 +61,11 @@ const startTimer = () => {
     let p1sec = 60;
     let p2sec = 60;
 
-    let timer = setInterval(function() {
+    let timerId = setInterval(function() {
         // Player 1.
         if (currentPlayer === 1) {
             if (playing) {
+                // Disable start button.
                 buttons[0].disabled = true;
                 p1time.minutes = parseInt(p1time.getMinutes('min1'), 10);
                 if (p1sec === 60) {
@@ -71,9 +76,12 @@ const startTimer = () => {
                 document.getElementById('sec1').textContent = padZero(p1sec);
                 document.getElementById('min1').textContent = padZero(p1time.minutes);
                 if (p1sec === 0) {
+                    // If minutes and seconds are zero stop timer with the clearInterval method.
                     if (p1sec === 0 && p1time.minutes === 0) {
+                        // Play a sound effect.
                         timesUp.play();
-                        clearInterval(timer);
+                        // Stop timer.
+                        clearInterval(timerId);
                         playing = false;
                     }
                     p1sec = 60;
@@ -91,16 +99,19 @@ const startTimer = () => {
                 document.getElementById('sec2').textContent = padZero(p2sec);
                 document.getElementById('min2').textContent = padZero(p2time.minutes);
                 if (p2sec === 0) {
+                    // If minutes and seconds are zero stop timer with the clearInterval method.
                     if (p2sec === 0 && p2time.minutes === 0) {
+                        // Play a sound effect.
                         timesUp.play();
-                        clearInterval(timer);
+                        // Stop timer.
+                        clearInterval(timerId);
                         playing = false;
                     }
                     p2sec = 60;
                 }
             }
         }
-    }, 1000)
+    }, 1000);
 }
 
 
